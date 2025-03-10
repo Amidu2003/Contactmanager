728d49fb1d515a2dfe27a9bc30c9295ee0ae4726
Amidu_Kaveesha <amidukaveesha@gmail.com>
2025-03-10 22:53:11 +0530

    Create DTO Class

--- src/main/java/org/Contactmanager/Contact.java
+++ src/main/java/org/Contactmanager/Contact.java
@@ -1,4 +1,38 @@
 package org.Contactmanager;
 
 public class Contact {
+    public String getName() {
+        return name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Contact(String name, String mobile, String email) {
+        this.name = name;
+        this.mobile = mobile;
+        this.email = email;
+    }
+
+     private String name;
+     private String mobile;
+     private String email;
+
 }
