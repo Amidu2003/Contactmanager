cca7a9bb3df0bcc30e4db4f4a2d2e45ac9b82aef
Amidu_Kaveesha <amidukaveesha@gmail.com>
2025-03-10 23:38:49 +0530

    Get All CMD test pass

--- src/main/java/org/contactmanager/Main.java
+++ src/main/java/org/contactmanager/Main.java
@@ -1,7 +1,15 @@
 package org.contactmanager;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Contact> contacts= FileHandler. readContactsFromFile();
+        for (Contact contat:contacts) {
+            System.out.println("name "+ contat.getName());
+            System.out.println("mobile "+ contat.getMobile());
+            System.out.println("Email "+ contat.getEmail());
+
+        }
     }
 }
