a45f8ae45d322418f6c86704af504af89efc13fa
Amidu_Kaveesha <amidukaveesha@gmail.com>
2025-03-11 08:42:12 +0530

    UI Implemented

--- src/main/java/org/contactmanager/ContactManager.java
+++
@@ -1,4 +0,0 @@
-package org.contactmanager;
-
-public class ContactManager {
-}
--- src/main/java/org/contactmanager/Main.java
+++ src/main/java/org/contactmanager/Main.java
@@ -1,15 +1,26 @@
 package org.contactmanager;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<Contact> contacts= FileHandler. readContactsFromFile();
-        for (Contact contat:contacts) {
-            System.out.println("name "+ contat.getName());
-            System.out.println("mobile "+ contat.getMobile());
-            System.out.println("Email "+ contat.getEmail());
 
-        }
-    }
+        JFrame frame = new JFrame("Contact manager");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,400);
+        frame.setLayout(new BorderLayout());
+
+        contactManager contactUI = new contactManager(contacts);
+        JScrollPane scrollPane = new JScrollPane(contactUI);
+        frame.add(scrollPane,BorderLayout.CENTER);
+
+        frame.setVisible(true);
+
+
+
+
+     }
 }
